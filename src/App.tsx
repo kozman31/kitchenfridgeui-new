0d4db3acb4669e0a6d7976b5d45a87b5b55ecc53
Elliott Kozulak <kozman31@gmail.com>
2019-11-17 18:22:15 -0500

    latest updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './nav-bar/navbar';
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <NavBar name='awesome' emote='!!!@!'></NavBar>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
+        <img src={logo}
+          className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Hello World! This is a test...
         </p>
         <a
           className="App-link"
